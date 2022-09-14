ce8cfef86c34e7a3eb1e8d2080a128dd6adc5365
Егор <egor.world.91@gmail.com>
2022-09-14 18:37:54 +0500

    third commit

--- egor/models.py
+++ egor/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class Order(models.Model):
-    order_dt = models.DateTimeField(auto_now=True)
+    order_dt = models.DateTimeField(auto_now=True, verbose_name="Дата")
     order_name = models.CharField(max_length=200, verbose_name='Имя')
     order_phone = models.CharField(max_length=200, verbose_name='Телефон')
 

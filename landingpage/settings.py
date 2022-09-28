29da6ffdd815e6cb56b189ba8cf858b82430b799
Егор <egor.world.91@gmail.com>
2022-09-28 14:06:39 +0500

    5th commit

--- landingpage/settings.py
+++ landingpage/settings.py
@@ -29,6 +29,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'price.apps.PriceConfig',
+    'cms.apps.CmsConfig',
     'egor.apps.EgorConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -111,10 +113,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'landingpage/static/',
+    BASE_DIR / "landingpage/static/",
 ]
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static ')
 
 MEDIA_URL = 'media/'
